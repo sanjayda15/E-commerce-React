5b785544769211d9607a13cf4a98968666671205
Sanjay Das <sanjayda15@gmail.com>
2021-05-04 16:46:56 +0530

    link added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      Hats Page
+    </h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
